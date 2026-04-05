d6a325dd20a1818586bdf9c4a227275561d8e8ec
Neeraj AY <neerajay04@gmail.com>
2026-04-05 19:18:40 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0 
+        right = len(height) - 1
+        lmax = 0
+        rmax = 0
+        totalvolume = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= lmax:
+                    lmax = height[left]
+                else:
+                    totalvolume += lmax - height[left] 
+                left += 1
+                                 
+            else:
+                if height[right] >= rmax:
+                    rmax = height[right]
+                else:
+                    totalvolume += rmax - height[right]
+                right -= 1
+        return totalvolume
+
+        
